a3a78e45268b0916842a040951f348f81ee3f8b2
Manikanth Movva <manikanthmovva@gmail.com>
2019-02-12 21:17:33 -0800

    added determine_z() to find z values given generated x and y values

--- face.py
+++ face.py
@@ -7,6 +7,7 @@ class Face():
       self.v3 = v3
       self.area = self.calculate_area()
       self.portion = -1    # -1 means not calculated
+      self.determine_z(v3[0],v3[1])
 
    def __repr__(self):
       return "\nvertices: (%s, %s, %s) area: %f"% (self.v1, self.v2, self.v3, self.area)
@@ -19,3 +20,18 @@ class Face():
       area = 0.5 * math.sqrt(((AB[1]*AC[2]-AB[2]*AC[1])**2) + ((AB[2]*AC[0]-AB[0]*AC[2])**2) + ((AB[0]*AC[1]-AB[1]*AC[0])**2)) 
       
       return (area)
+    
+   def determine_z(self, x, y):
+      # given x and y, determines z on the plane
+      AB = numpy.subtract(self.v2,self.v1)
+      AC = numpy.subtract(self.v3,self.v1)
+      
+      (r,s,t) = (AB[1]*AC[2]-AB[2]*AC[1]) , (AB[2]*AC[0]-AB[0]*AC[2]) , (AB[0]*AC[1]-AB[1]*AC[0])
+   
+      if t == 0:
+        raise ValueError("cannot determine z from x and y for this plane", r, s, t)
+        
+      k = r*self.v1[0] + s*self.v1[1] + t*self.v1[2] 
+      
+      𝑧 =(1/t)*((r*self.v1[0])+(𝑠*self.v1[1])+(t*self.v1[2])-(r*x)-(s*y))
+      print(z)
